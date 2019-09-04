d79daf9a915490786b4fc6eb15c8f0d765e6d5e4
Don Bennett <dbennett@google.com>
2019-09-04 14:51:09 -0700

    checkpoint

--- scripts/riders.py
+++ scripts/riders.py
@@ -2,22 +2,7 @@
 
 import requests
 
-def get_frame(frame):
-    print("Requesting rider info for frame %s" % frame)
-
-    payload = {
-        "max": 100,
-        "failonmax": 1,
-        "total": 1,
-        "appid": "592479603a4c925a288b4567",
-        "token": "B714F968F812FEDCA917",
-        "search": frame,
-        "source": "webtracker",
-        }
-
-    r = requests.post("https://api.rtrt.me/events/PBP-2019/profiles", data=payload)
-    riderData = r.json()["list"][0]
-    print(riderData)
+def get_rider_times(pid):
 
     payload = {
         "appid": "592479603a4c925a288b4567",
@@ -30,13 +15,41 @@ def get_frame(frame):
         "source": "webtracker",
     }
 
-    response = requests.post("https://api.rtrt.me/events/PBP-2019/profiles/%s/splits" % riderData["pid"], data=payload)
-    print (response.text)
+    return requests.post("https://api.rtrt.me/events/PBP-2019/profiles/%s/splits" % pid, data=payload).json()
 
+def get_rider_info(frame):
+    print("Requesting rider info for frame %s" % frame)
+
+    payload = {
+        "max": 100,
+        "failonmax": 1,
+        "total": 1,
+        "appid": "592479603a4c925a288b4567",
+        "token": "B714F968F812FEDCA917",
+        "search": frame,
+        "source": "webtracker",
+        }
+
+    r = requests.post("https://api.rtrt.me/events/PBP-2019/profiles", data=payload).json()
+    if "list" in r:
+        return r["list"][0]
+
+    return None
 
 def main():
 
-    info = get_frame("V041")
-    print(info)
+    if False:
+        for i in range(1):
+            frame = "A%03d" % (i+1)
+            print (frame)
+            get_rider_info(frame)
+
+    rider_info = get_rider_info("V041")
+    rider_times = None
+    if rider_info:
+        rider_times = get_rider_times(rider_info["pid"])
+    print(rider_info)
+    print(rider_times)
+
 if __name__ == "__main__":
     main()
